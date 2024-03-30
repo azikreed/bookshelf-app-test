94aafc19779de176631778b19a0aacc10647203a
azikreed <azikreed@gmail.com>
2024-03-30 10:52:51 +0300

    [+] settings to use vite with mui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended', // Добавляем это расширение, если вы используете React
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -21,4 +25,18 @@ module.exports = {
     "react/prop-types": [0],
     indent: ["error", "tab"],
   },
-}
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { 'args': 'none' }],
+      },
+    },
+  ],
+};
