f25d4f2eb9790d1ffa8820669737b555e4188be9
azikreed <azikreed@gmail.com>
2024-03-30 12:59:24 +0300

    [+] AuthLayout

---
+++ src/layouts/Auth/AuthLayout.tsx
@@ -0,0 +1,29 @@
+import { styled } from "@mui/system";
+import { Theme } from "@mui/material/styles";
+import { Outlet } from "react-router-dom";
+
+export const AuthLayout = () => {
+  const Layout = styled("div")(({ theme }: { theme: Theme }) => ({
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const Content = styled("div")(({ theme }: { theme: Theme }) => ({
+    padding: "48px 28px",
+    background: theme.palette.primary.light,
+    borderRadius: "12px",
+    width: "430px",
+    boxShadow: '0 4px 32px 0 rgba(51, 51, 51, 0.08)',
+  }));
+
+  return (
+    <Layout>
+      <Content>
+        <Outlet />
+      </Content>
+    </Layout>
+  );
+};
