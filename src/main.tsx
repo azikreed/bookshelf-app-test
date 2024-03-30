db428a85e697b01631637b417200a68a9fd940d2
azikreed <azikreed@gmail.com>
2024-03-30 14:39:09 +0300

    [+] error page

--- src/main.tsx
+++ src/main.tsx
@@ -2,29 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline, GlobalStyles } from "@mui/material";
 import { ThemeProvider } from "@mui/system";
-import mainBack from "/background.png";
+import mainBack from "/main_back.png";
 import theme from "./theme";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { AuthLayout } from "./layouts/Auth/AuthLayout";
 import { Login } from "./pages/Login/Login";
 import { Register } from "./pages/Register/Register";
+import { ErrorPage } from "./pages/Error/Error";
+import { Layout } from "./layouts/Layout/Layout";
 
 const router = createBrowserRouter([
   {
-    path:'/auth',
-    element: <AuthLayout/>,
+    path: '/',
+    element: <Layout />,
+    children: [
+      // {
+      //   path: 'login',
+      //   element: <Login />,
+      // },
+      // {
+      //   path: 'register',
+      //   element: <Register />,
+      // },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
     children: [
       {
-				path:'login',
-				element: <Login/>
-			},
-			{
-				path:'register',
-				element: <Register/>
-			}
-    ]
-  }
-])
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      // Добавьте другие маршруты здесь
+    ],
+  },
+  {
+    path: '/*',
+    element: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -37,9 +58,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             backgroundColor: "#fefefe",
             backgroundImage: `url(${mainBack})`,
             backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundAttachment: "fixed",
+            backgroundSize: "unset",
+            backgroundAttachment: "",
             fontFamily: "Montserrat, sans-serif",
             margin: 0,
             padding: 0
