1fd05956d712e72853d44f637ca71c622820179f
azikreed <azikreed@gmail.com>
2024-03-30 10:53:36 +0300

    [+] theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CssBaseline, GlobalStyles } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import mainBack from "/main_back.png";
+import theme from "./theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          body: {
+            backgroundColor: "#fff",
+            backgroundImage: `url(${mainBack})`,
+            backgroundRepeat: "no-repeat",
+            fontFamily: "Montserrat, sans-serif",
+          },
+        }}
+      />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
