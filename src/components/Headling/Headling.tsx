7bb81cbe436ef5105c333d064ee08b1c6a7b440f
azikreed <azikreed@gmail.com>
2024-03-31 20:07:47 +0300

    [+] appearance props

--- src/components/Headling/Headling.tsx
+++ src/components/Headling/Headling.tsx
@@ -2,13 +2,14 @@ import { Typography } from "@mui/material";
 import { HeadlingProps } from "./Headling.props";
 import { Theme, styled } from "@mui/material/styles";
 
-const StyledTypography = styled(Typography)(({ theme }: { theme: Theme }) => ({
-  color: theme?.palette?.secondary?.main,
-  fontSize: "36px !important",
-  fontWeight: 700,
-  fontFamily: theme?.typography?.fontFamily,
-}));
 
-export const Headling = ({ children }: HeadlingProps) => {
+export const Headling = ({ children, appearance }: HeadlingProps) => {
+  const StyledTypography = styled(Typography)(({ theme }: { theme: Theme }) => ({
+    color: theme?.palette?.secondary?.main,
+    fontSize: appearance === 'small' ? "16px !important" : "36px !important",
+    fontWeight: appearance === 'small' ? 600 : 700,
+    fontFamily: theme?.typography?.fontFamily,
+  }));
+
   return <StyledTypography variant="h1">{children}</StyledTypography>;
 };
